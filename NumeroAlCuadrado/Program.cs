6d61ebb19c8b31fa5df8701562e72f39ef3932b8
ObedVPSM256 <105815117+ObedVPSM256@users.noreply.github.com>
2022-05-25 11:54:07 -0700

    Add files via upload

---
+++ NumeroAlCuadrado/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroAlCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int nume;
+            try
+            {
+                Console.Write("Introduzca el número que desea elevar al cuadrado: ");
+                nume = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(nume * nume);
+                Console.ReadLine();
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
